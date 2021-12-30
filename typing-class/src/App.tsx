1ef42dc4a194a1e501cf153345222c98b2da89ae
Divyarajsinh <divyarajsinh.rana@archesoftronix.com>
2021-12-30 14:18:25 +0530

    polymorphic done

--- typing-class/src/App.tsx
+++ typing-class/src/App.tsx
@@ -8,22 +8,9 @@ import Toast from "./component/TemplateLiteral/Toast"
 import Button from "./component/HTML/Button"
 import Input from "./component/HTML/Input"
 import CustomComponent from "./component/HTML/CustomComponent"
-const data = [{
-  id :1,
-  first : "premraj",
-  last :"jadav"
-},
-{
-id :2,
-first : "dhaval",
-last :"raj"
-},
-{
-id :3,
-first : "hardik",
-last :"rana"
-}
-]
+import { data } from "./data"
+import Test from './component/polymorphic/Test'
+
 const App = () => {
   return (
     <div className="App">
@@ -32,9 +19,18 @@ const App = () => {
       {/* <List items={data} handleClick={(item)=>console.log(item)} /> */}
       {/* <RandomNumber value={11}  isPositive /> */}
       {/* <Toast  position="center"/> */}
-      <Button variant="primary" onClick={()=> console.log("Hello world")} > primary button</Button>
+      {/* <Button variant="primary" onClick={()=> console.log("Hello world")} > primary button</Button>
       <Input onChange = {(e)=>console.log(e.target.value)}/>
-      <CustomComponent  message="My name iss ram"/>
+      <CustomComponent  message="My name iss ram"/> */}
+       <Test size='lg' as='h1'>
+        Heading
+      </Test>
+      <Test size='md' as='p'>
+        Paragraph
+      </Test>
+      <Test size='sm' color='secondary' as='label' htmlFor='someId'>
+        Label
+      </Test>
     </div>
   )
 }
---
+++ typing-class/src/data.ts
@@ -0,0 +1,18 @@
+const data = [{
+    id :1,
+    first : "premraj",
+    last :"jadav"
+  },
+  {
+  id :2,
+  first : "dhaval",
+  last :"raj"
+  },
+  {
+  id :3,
+  first : "hardik",
+  last :"rana"
+  }
+  ]
+
+  export {data}
